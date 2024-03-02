922dbbaf77cbe41c8f5d181d3524b01418a00b2a
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-03-02 17:45:06 +0200

    Moving the dummy data in the starting App component and render the Card component for each element

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import Card from './components/Card/Card';
 
+const dummyData = [
+	{
+		name: 'Test name1',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+	{
+		name: 'Test name2',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+    {
+		name: 'Test name3',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+    {
+		name: 'Test name4',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+    {
+		name: 'Test name5',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+    {
+		name: 'Test name6',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
+];
+
 const App = () => {
 	return (
 		<main>
-			<Card />
+            {dummyData.map(x => <Card key={x.name} person={x} />)}
 		</main>
 	);
 };
