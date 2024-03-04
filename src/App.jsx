b98002bac2bfdf448432583114f3e1ffd7b15abc
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-03-04 10:23:48 +0200

    Add func to cut long string and return substring with dots

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,11 @@ const dummyData = [
 		email: 'testemail@gmail.com',
 		phone: '+35923434343',
 	},
+    {
+		name: 'Test name7',
+		email: 'testemail@gmail.com',
+		phone: '+35923434343',
+	},
 ];
 
 const App = () => {
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import classes from './Contact.module.css';
 
 const Contact = ({ contact }) => {
+    const cutString = (str, maxLength) => {
+        return str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
+    };
+
 	return (
-		<div className={classes.wrapper}>
-			<div className={classes.heading}>{contact.name}</div>
-			<div>{contact.email}</div>
-			<div>{contact.phone}</div>
+		<div className={classes.contact}>
+			<div className={classes.name}>{cutString(contact.name, 16)}</div>
+			<div>{cutString(contact.email, 16)}</div>
+			<div>{cutString(contact.phone, 14)}</div>
 		</div>
 	);
 };
