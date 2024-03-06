860025a0d2f98a1140107b49eacb4a03f9819465
Dimitar Stefanov <dimitarnew9@gmail.com>
2024-03-06 10:42:59 +0200

    Add state for inputs in contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,15 +1,43 @@
+import { useState } from 'react';
+
 import classes from './ContactForm.module.css';
 
 const ContactForm = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    
+	const onNameChangeHandler = (event) => {
+		setName(event.target.value);
+	};
+
+    const onEmailChangeHandler = (event) => {
+        setEmail(event.target.value);
+    };
+
 	return (
 		<form className={classes.form}>
 			<div className={classes['form-control']}>
-				<input type='text' id='name' className={classes.input} required />
-                <div className={classes.placeholder}>Enter your name</div>
+				<input
+					type='text'
+					id='name'
+					className={classes.input}
+					onChange={onNameChangeHandler}
+					required
+				/>
+				<div className={classes.placeholder}>Name</div>
 			</div>
-            <div className={classes['form-control']}>
-				<input type='text' id='email' className={classes.input} required />
-                <div className={classes.placeholder}>Enter your email</div>
+			<div className={classes['form-control']}>
+				<input
+					type='text'
+					id='email'
+					className={classes.input}
+                    onChange={onEmailChangeHandler}
+					required
+				/>
+				<div className={classes.placeholder}>Email</div>
+			</div>
+			<div>
+				<button className={classes.button}>Add</button>
 			</div>
 		</form>
 	);
